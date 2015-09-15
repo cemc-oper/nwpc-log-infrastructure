1a10b7a47bedf0f437cd7669f6af880605770840
perillaroc <perillaroc@gmail.com>
2015-09-15 11:08:54 +0800

    ADD: zookeeper host and port.

--- kafka/create_kafka_topic.sh
+++ kafka/create_kafka_topic.sh
@@ -2,8 +2,14 @@
 set -x
 export KAFKA_BASE=/home/vagrant/app/kafka_2.8.0-0.8.1.1
 
-$KAFKA_BASE/bin/kafka-topics.sh --create --zookeeper 10.28.32.175:2181 --replication-factor 1 --partitions 1 --topic collector
-$KAFKA_BASE/bin/kafka-topics.sh --create --zookeeper 10.28.32.175:2181 --replication-factor 1 --partitions 1 --topic collector_log
-$KAFKA_BASE/bin/kafka-topics.sh --create --zookeeper 10.28.32.175:2181 --replication-factor 1 --partitions 1 --topic collector_error_log
+zookeeper_host=10.28.32.175
+zookeeper_port=2181
+
+$KAFKA_BASE/bin/kafka-topics.sh --create --zookeeper ${zookeeper_host}:${zookeeper_port} \
+    --replication-factor 1 --partitions 1 --topic collector
+$KAFKA_BASE/bin/kafka-topics.sh --create --zookeeper ${zookeeper_host}:${zookeeper_port} \
+    --replication-factor 1 --partitions 1 --topic collector_log
+$KAFKA_BASE/bin/kafka-topics.sh --create --zookeeper ${zookeeper_host}:${zookeeper_port} \
+    --replication-factor 1 --partitions 1 --topic collector_error_log
 
 set +x
