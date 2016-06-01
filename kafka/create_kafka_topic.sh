4fe32a3619d4da816ceb49dd88d7607442b773c0
perillaroc <perillaroc@gmail.com>
2016-06-01 13:16:04 +0800

    CHANGE: zk's ip

--- kafka/create_kafka_topic.sh
+++ kafka/create_kafka_topic.sh
@@ -2,7 +2,7 @@
 set -x
 export KAFKA_BASE=/home/vagrant/app/kafka/kafka_2.10-0.8.2.0
 
-zookeeper_host=10.28.32.175
+zookeeper_host=192.168.99.100
 zookeeper_port=2181
 
 $KAFKA_BASE/bin/kafka-topics.sh --create --zookeeper ${zookeeper_host}:${zookeeper_port} \
